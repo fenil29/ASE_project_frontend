3ca379caa24a25e4618f1eca23cd750ca300f6ce
Jigar Mehta <jigarmehta3998@gmail.com>
2021-11-24 21:52:50 -0500

    Added Logout functionality and removed time domain

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import * as eva from '@eva-design/eva';
-import { StyleSheet} from 'react-native';
-import { Layout, Text, RangeDatepicker } from '@ui-kitten/components';
+import React from "react";
+import * as eva from "@eva-design/eva";
+import { StyleSheet } from "react-native";
+import { Layout, Text, RangeDatepicker } from "@ui-kitten/components";
 
-import PhotoGrid from '../components/PhotoGrid'
-import Graph from '../components/Graph';
+import PhotoGrid from "../components/PhotoGrid";
+import Graph from "../components/Graph";
 
-function Home(){
-    const [range, setRange] = React.useState({});
-    
+function Home() {
+  const [range, setRange] = React.useState({});
 
-    return (
-        <Layout style={styles.container}>
-            {/* <Layout style={styles.dateBar} > */}
-                
-                <Layout style={styles.datePicker}>
+  return (
+    <Layout style={styles.container}>
+      {/* <Layout style={styles.dateBar} > */}
+
+      {/* <Layout style={styles.datePicker}>
                 <Text style={{
                     fontWeight: 'bold',
                     paddingTop: 10
@@ -25,61 +24,64 @@ function Home(){
                         range={range}
                         onSelect={nextRange => setRange(nextRange)}
                     />
-                </Layout>
-            {/* </Layout> */}
-            <Layout style={styles.layout} level='2'>
-                <Text style={{
-                    fontWeight: 'bold',
-                    paddingTop: 15
-                    
-                }}>Recently Detected Faces</Text>
-                <PhotoGrid />
-            </Layout>
+                </Layout> */}
+      {/* </Layout> */}
+      <Layout style={styles.layout} level="2">
+        <Text
+          style={{
+            fontWeight: "bold",
+            paddingTop: 15,
+          }}
+        >
+          Recently Detected Faces
+        </Text>
+        <PhotoGrid />
+      </Layout>
 
-            <Layout style={styles.layout} level='2'>
-                <Text style={{
-                    fontWeight: 'bold',   
-                    paddingTop: 15                 
-                }}>Activity Log</Text>
-                <Graph />
-            </Layout>          
-        </Layout>
-        
-        );    
+      <Layout style={styles.layout} level="2">
+        <Text
+          style={{
+            fontWeight: "bold",
+            paddingTop: 15,
+          }}
+        >
+          Activity Log
+        </Text>
+        <Graph />
+      </Layout>
+    </Layout>
+  );
 }
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-    },
-    date:{
-        width: 130,
-        alignItems: 'center',
-        position: 'relative',
-        left: 110,
-        
-    },
-    datePicker: {
-        flex: 0.2,  
-        flexDirection: 'row',  
-        position: 'relative',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        // top: 25,
-        left: 20,
-
-    },
-    gridView: {
-        marginTop: 10,
-        flex: 1,
-    },    
-    layout: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    
-  });
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  date: {
+    width: 130,
+    alignItems: "center",
+    position: "relative",
+    left: 110,
+  },
+  datePicker: {
+    flex: 0.2,
+    flexDirection: "row",
+    position: "relative",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    // top: 25,
+    left: 20,
+  },
+  gridView: {
+    marginTop: 10,
+    flex: 1,
+  },
+  layout: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
