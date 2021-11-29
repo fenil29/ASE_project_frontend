f2ef116bd70c0cab36db2e1404a5c99321ec7780
fenil29 <fenilkaneria@gmail.com>
2021-11-29 17:44:17 -0500

    add extra vaccine info in face camera screen

--- screens/FaceCamera.js
+++ screens/FaceCamera.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useRef,useContext } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import {
   StyleSheet,
   Text,
@@ -16,174 +16,172 @@ import * as ImageManipulator from "expo-image-manipulator";
 
 import { GlobalContext } from "../context/GlobalState";
 
-
 personData = {
-  "d9cad8113495924deb71866fdf20f592": "Amir",
+  d9cad8113495924deb71866fdf20f592: "Amir",
   "13277a9fa5f57a62e12448cc8a720693": "akshay",
-  "c77c44564de4b43738e195c547e68d16": "alia",
-  "dc1eeec115d614d8745e7503987fcbc9": "Amitabh",
+  c77c44564de4b43738e195c547e68d16: "alia",
+  dc1eeec115d614d8745e7503987fcbc9: "Amitabh",
   "6c1a31b03cf112caf4231b3a1aafd42c": "Hritik",
-  "d775c1db22ae162c82c2a62c6a095b58": "Kareena",
-  "c937e97caac871dc9c6a9fbb81adc028": "Kriti",
+  d775c1db22ae162c82c2a62c6a095b58: "Kareena",
+  c937e97caac871dc9c6a9fbb81adc028: "Kriti",
   "9053b9da50c608a5022d5f5f4c8763d6": "Salman",
   "1ad24f20921d0c409ae922cb5746b14c": "Shahrukh",
   "72056b0df88ab5cdc0866b9c56f26966": "Saif Ali khan",
   "6022d052cce8924ff2d1363c1eeafdff": "Jigar",
 };
 
-vaccineData=[
+vaccineData = [
   {
-    "dateOfBirth" : "1972-01-22",
-    "fullName" : "Amir",
-    "faceId" : "d9cad8113495924deb71866fdf20f592",
-    "status" : "FULLY_VACCINATED",
-    "vaccineDoses": [
+    dateOfBirth: "1972-01-22",
+    fullName: "Amir",
+    faceId: "d9cad8113495924deb71866fdf20f592",
+    status: "FULLY_VACCINATED",
+    vaccineDoses: [
       {
-        "vaccineName": "AstraZeneca",
-        "date": "2021-04-20",
-        "lotNumber": "731648"
+        vaccineName: "AstraZeneca",
+        date: "2021-04-20",
+        lotNumber: "731648",
       },
       {
-        "vaccineName": "AstraZeneca",
-        "date": "2021-07-30",
-        "lotNumber": "788134"
-      }
-    ]
+        vaccineName: "AstraZeneca",
+        date: "2021-07-30",
+        lotNumber: "788134",
+      },
+    ],
   },
   {
-    "dateOfBirth" : "1970-05-12",
-    "fullName" : "Akshay",
-    "faceId" : "13277a9fa5f57a62e12448cc8a720693",
-    "status" : "NOT_VACCINATED",
-    "vaccineDoses": []
+    dateOfBirth: "1970-05-12",
+    fullName: "Akshay",
+    faceId: "13277a9fa5f57a62e12448cc8a720693",
+    status: "NOT_VACCINATED",
+    vaccineDoses: [],
   },
   {
-    "dateOfBirth" : "1999-06-11",
-    "fullName" : "Alia",
-    "faceId" : "c77c44564de4b43738e195c547e68d16",
-    "status" : "FULLY_VACCINATED",
-    "vaccineDoses": [
+    dateOfBirth: "1999-06-11",
+    fullName: "Alia",
+    faceId: "c77c44564de4b43738e195c547e68d16",
+    status: "FULLY_VACCINATED",
+    vaccineDoses: [
       {
-        "vaccineName": "Pfizer",
-        "date": "2021-06-28",
-        "lotNumber": "500648"
+        vaccineName: "Pfizer",
+        date: "2021-06-28",
+        lotNumber: "500648",
       },
       {
-        "vaccineName": "Pfizer",
-        "date": "2021-09-01",
-        "lotNumber": "551184"
-      }
-    ]
+        vaccineName: "Pfizer",
+        date: "2021-09-01",
+        lotNumber: "551184",
+      },
+    ],
   },
   {
-    "dateOfBirth" : "1960-02-12",
-    "fullName" : "Amitabh",
-    "faceId" : "dc1eeec115d614d8745e7503987fcbc9",
-    "status" : "PARTIALLY_VACCINATED",
-    "vaccineDoses": [
+    dateOfBirth: "1960-02-12",
+    fullName: "Amitabh",
+    faceId: "dc1eeec115d614d8745e7503987fcbc9",
+    status: "PARTIALLY_VACCINATED",
+    vaccineDoses: [
       {
-        "vaccineName": "AstraZeneca",
-        "date": "2021-03-04",
-        "lotNumber": "600648"
-      }
-    ]
+        vaccineName: "AstraZeneca",
+        date: "2021-03-04",
+        lotNumber: "600648",
+      },
+    ],
   },
   {
-    "dateOfBirth" : "1975-05-23",
-    "fullName" : "Hritik",
-    "faceId" : "6c1a31b03cf112caf4231b3a1aafd42c",
-    "status" : "NOT_VACCINATED",
-    "vaccineDoses": []
+    dateOfBirth: "1975-05-23",
+    fullName: "Hritik",
+    faceId: "6c1a31b03cf112caf4231b3a1aafd42c",
+    status: "NOT_VACCINATED",
+    vaccineDoses: [],
   },
   {
-    "dateOfBirth" : "1982-05-22",
-    "fullName" : "Kareena",
-    "faceId" : "d775c1db22ae162c82c2a62c6a095b58",
-    "status" : "FULLY_VACCINATED",
-    "vaccineDoses": [
+    dateOfBirth: "1982-05-22",
+    fullName: "Kareena",
+    faceId: "d775c1db22ae162c82c2a62c6a095b58",
+    status: "FULLY_VACCINATED",
+    vaccineDoses: [
       {
-        "vaccineName": "AstraZeneca",
-        "date": "2021-05-15",
-        "lotNumber": "600648"
+        vaccineName: "AstraZeneca",
+        date: "2021-05-15",
+        lotNumber: "600648",
       },
       {
-        "vaccineName": "AstraZeneca",
-        "date": "2021-08-26",
-        "lotNumber": "655184"
-      }
-    ]
+        vaccineName: "AstraZeneca",
+        date: "2021-08-26",
+        lotNumber: "655184",
+      },
+    ],
   },
   {
-    "dateOfBirth" : "1995-03-12",
-    "fullName" : "Kriti",
-    "faceId" : "c937e97caac871dc9c6a9fbb81adc028",
-    "status" : "NOT_VACCINATED",
-    "vaccineDoses": []
+    dateOfBirth: "1995-03-12",
+    fullName: "Kriti",
+    faceId: "c937e97caac871dc9c6a9fbb81adc028",
+    status: "NOT_VACCINATED",
+    vaccineDoses: [],
   },
   {
-    "dateOfBirth" : "1969-07-05",
-    "fullName" : "Salman",
-    "faceId" : "9053b9da50c608a5022d5f5f4c8763d6",
-    "status" : "PARTIALLY_VACCINATED",
-    "vaccineDoses": [
+    dateOfBirth: "1969-07-05",
+    fullName: "Salman",
+    faceId: "9053b9da50c608a5022d5f5f4c8763d6",
+    status: "PARTIALLY_VACCINATED",
+    vaccineDoses: [
       {
-        "vaccineName": "Pfizer",
-        "date": "2021-04-10",
-        "lotNumber": "520648"
-      }
-    ]
+        vaccineName: "Pfizer",
+        date: "2021-04-10",
+        lotNumber: "520648",
+      },
+    ],
   },
   {
-    "dateOfBirth" : "1967-08-09",
-    "fullName" : "Shahrukh",
-    "faceId" : "1ad24f20921d0c409ae922cb5746b14c",
-    "status" : "FULLY_VACCINATED",
-    "vaccineDoses": [
+    dateOfBirth: "1967-08-09",
+    fullName: "Shahrukh",
+    faceId: "1ad24f20921d0c409ae922cb5746b14c",
+    status: "FULLY_VACCINATED",
+    vaccineDoses: [
       {
-        "vaccineName": "Moderna",
-        "date": "2021-03-28",
-        "lotNumber": "320648"
+        vaccineName: "Moderna",
+        date: "2021-03-28",
+        lotNumber: "320648",
       },
       {
-        "vaccineName": "Moderna",
-        "date": "2021-05-01",
-        "lotNumber": "341184"
-      }
-    ]
+        vaccineName: "Moderna",
+        date: "2021-05-01",
+        lotNumber: "341184",
+      },
+    ],
   },
   {
-    "dateOfBirth" : "1973-01-10",
-    "fullName" : "Saif Ali khan",
-    "faceId" : "72056b0df88ab5cdc0866b9c56f26966",
-    "status" : "FULLY_VACCINATED",
-    "vaccineDoses": [
+    dateOfBirth: "1973-01-10",
+    fullName: "Saif Ali khan",
+    faceId: "72056b0df88ab5cdc0866b9c56f26966",
+    status: "FULLY_VACCINATED",
+    vaccineDoses: [
       {
-        "vaccineName": "Moderna",
-        "date": "2021-04-28",
-        "lotNumber": "330648"
+        vaccineName: "Moderna",
+        date: "2021-04-28",
+        lotNumber: "330648",
       },
       {
-        "vaccineName": "Moderna",
-        "date": "2021-06-05",
-        "lotNumber": "358184"
-      }
-    ]
+        vaccineName: "Moderna",
+        date: "2021-06-05",
+        lotNumber: "358184",
+      },
+    ],
   },
   {
-    "dateOfBirth" : "1998-01-11",
-    "fullName" : "Jigar",
-    "faceId" : "6022d052cce8924ff2d1363c1eeafdff",
-    "status" : "PARTIALLY_VACCINATED",
-    "vaccineDoses": [
+    dateOfBirth: "1998-01-11",
+    fullName: "Jigar",
+    faceId: "6022d052cce8924ff2d1363c1eeafdff",
+    status: "PARTIALLY_VACCINATED",
+    vaccineDoses: [
       {
-        "vaccineName": "AstraZeneca",
-        "date": "2021-05-02",
-        "lotNumber": "630648"
-      }
-    ]
-  }   
-]
-
+        vaccineName: "AstraZeneca",
+        date: "2021-05-02",
+        lotNumber: "630648",
+      },
+    ],
+  },
+];
 
 export default function FaceCamera() {
   const contextStore = useContext(GlobalContext);
@@ -197,6 +195,7 @@ export default function FaceCamera() {
   const [vaccineInfo, setVaccineInfo] = useState({});
   const [loading, setLoading] = useState(false);
   const [isVaccinated, setIsVaccinated] = useState(false);
+  const [notDetected, setNotDetected] = useState(true);
 
   //   let camera;
   useEffect(() => {
@@ -218,7 +217,7 @@ export default function FaceCamera() {
     // console.log(cameralocal)
 
     if (!cameraRef) return;
-    const options = { quality: 0.7, base64: true,skipProcessing: true };
+    const options = { quality: 0.7, base64: true, skipProcessing: true };
     let photo = await cameraRef.current.takePictureAsync(options);
     // return;
     photo = await ImageManipulator.manipulateAsync(
@@ -243,7 +242,14 @@ export default function FaceCamera() {
         // console.log(res);
         setLoading(false);
         console.log(res2.data);
-
+        setNotDetected(false);
+        if (
+          res2.data.faces.length == 0 ||
+          (res2.data.results.length > 0 &&
+            res2.data.results[0].confidence < res2.data.thresholds["1e-5"])
+        ) {
+          setNotDetected(true);
+        }
         if (res2.data.faces && res2.data.faces.length > 0) {
           setFaceBox(res2.data.faces[0].face_rectangle);
           // console.log(res.data.faces[0].face_rectangle);
@@ -254,20 +260,28 @@ export default function FaceCamera() {
           ) {
             setVaccineInfo({
               isVaccinated: true,
-              face_token: res2.data.results[0].face_token,
+              info: vaccineData.find(
+                (x) => x.faceId == res2.data.results[0].face_token
+              ),
             });
-            setIsVaccinated(false)
-        contextStore.appendPhoto([photo,false]);
+            setIsVaccinated(false);
+            contextStore.appendPhoto([photo, false]);
 
-            console.log(vaccineData.find(x=>x.faceId==res2.data.results[0].face_token));
-            if(vaccineData.find(x=>x.faceId==res2.data.results[0].face_token)){
-              if(vaccineData.find(x=>x.faceId==res2.data.results[0].face_token).status=="FULLY_VACCINATED")
-              {setIsVaccinated(true);
-        contextStore.appendPhoto([photo,true]);
-              
+            // console.log(vaccineData.find(x=>x.faceId==res2.data.results[0].face_token));
+            if (
+              vaccineData.find(
+                (x) => x.faceId == res2.data.results[0].face_token
+              )
+            ) {
+              if (
+                vaccineData.find(
+                  (x) => x.faceId == res2.data.results[0].face_token
+                ).status == "FULLY_VACCINATED"
+              ) {
+                setIsVaccinated(true);
+                contextStore.appendPhoto([photo, true]);
               }
             }
-            
           }
         }
 
@@ -280,18 +294,14 @@ export default function FaceCamera() {
           console.log(err2.response.data); // => the response payload
         }
       });
-
   }
 
   return (
     <View style={{ height: "100%" }}>
       <View style={{ height: "30%", width: "30%", display: "none" }}>
-        <Camera
-          type={type}
-          ref={cameraRef}
-        />
-        
+        <Camera type={type} ref={cameraRef} />
       </View>
+
       <TouchableOpacity
         style={{ position: "absolute", top: 20, left: 20, zIndex: 10 }}
         onPress={() => {
@@ -326,6 +336,7 @@ export default function FaceCamera() {
             //   flex: 1,
             }}
           /> */}
+
           <ImageBackground
             source={{ uri: capturedImage && capturedImage.uri }}
             // style={{ width: "100%" }}
@@ -336,38 +347,88 @@ export default function FaceCamera() {
             //   height:100,
             // }}
           />
-          {faceBox && faceBox.left && (
-            <View
+          {notDetected && (
+            <Text
               style={{
-                borderColor: isVaccinated?"green":"red",
-                borderStyle: "solid",
-                borderWidth: 4,
-                // padding: 10,
-                margin: 10,
-                borderRadius: 5,
+                color: "#6b7326",
+                fontSize: 20,
                 position: "absolute",
-                // top: 10, left: 10,
-                //  left: `10%`,
-                // right:`10%`,
-                // top: `10%`,
-                // bottom: `10%`,
-                left: `${Number(faceBox.left) / (capturedImage.width/100)}%`,
-                right:`${ 100-(Number(faceBox.left) / (capturedImage.width/100) + Number(faceBox.width) / (capturedImage.width/100))}%`,
-                top: `${Number(faceBox.top) / (capturedImage.height/100)}%`,
-                bottom: `${100-(Number(faceBox.top) / (capturedImage.height/100) + Number(faceBox.height) /  (capturedImage.height/100))}%`,
+                bottom: 20,
+                left: 20,
+                zIndex: 10,
+                width: "90%",
+                backgroundColor: "white",
+                padding: 10,
+                paddingLeft: 20,
+                paddingRight: 20,
+                // borderRadius:50,
+                // shadowRadius:10,
               }}
             >
-              {vaccineInfo.face_token && (
+              Not Found !!!
+            </Text>
+          )}
+          {faceBox && faceBox.left && (
+            <>
+              {!notDetected && vaccineInfo.info && (
                 <Text
                   style={{
-                    color: "red",
-                    fontSize:20
+                    color: isVaccinated ? "green" : "red",
+                    fontSize: 20,
+                    position: "absolute",
+                    bottom: 20,
+                    left: 20,
+                    zIndex: 10,
+                    width: "90%",
+                    backgroundColor: "white",
+                    padding: 10,
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                    // borderRadius:50,
+                    // shadowRadius:10,
                   }}
                 >
-                  {personData[vaccineInfo.face_token]}
+                  Name: {vaccineInfo.info.fullName}
+                  {"\n"}
+                  Vaccine Status:{vaccineInfo.info.status}
                 </Text>
               )}
-            </View>
+
+              <View
+                style={{
+                  borderColor: isVaccinated ? "green" : "red",
+                  borderStyle: "solid",
+                  borderWidth: 4,
+                  // padding: 10,
+                  margin: 10,
+                  borderRadius: 5,
+                  position: "absolute",
+                  // top: 10, left: 10,
+                  //  left: `10%`,
+                  // right:`10%`,
+                  // top: `10%`,
+                  // bottom: `10%`,
+                  left: `${Number(faceBox.left) / (capturedImage.width/100)}%`,
+                  right:`${ 100-(Number(faceBox.left) / (capturedImage.width/100) + Number(faceBox.width) / (capturedImage.width/100))}%`,
+                  top: `${Number(faceBox.top) / (capturedImage.height/100)}%`,
+                  bottom: `${100-(Number(faceBox.top) / (capturedImage.height/100) + Number(faceBox.height) /  (capturedImage.height/100))}%`,
+                }}
+              >
+                {!notDetected && vaccineInfo.info && (
+                  <Text
+                    style={{
+                      color: isVaccinated ? "green" : "red",
+                      fontSize: 25,
+                      fontWeight: "700",
+                      // paddingLeft:10,
+                      top: -35,
+                    }}
+                  >
+                    {vaccineInfo.info.fullName}
+                  </Text>
+                )}
+              </View>
+            </>
           )}
         </>
       )}
